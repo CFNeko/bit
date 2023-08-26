7dcc0ee5f27ff6fe6126a51e182305332dec3220
FrankCori <129750838+FrankCori@users.noreply.github.com>
2023-08-26 16:34:39 +0800

    Doctor
    Frame of Doctor

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
-#include "registerweb.h"
+#include "mainwindow.h"
+#include "doctorpage.h"
+#include "recorditempage.h"
 
 #include <QApplication>
+#include <QtSql>
+#include <QMessageBox>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    RegisterWeb w;
-    w.show();
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("/home/user/Qtworks/qq_tiyanfu/database/database.db");
+    if (!db.open()) {
+        QMessageBox::critical(nullptr, "Error", "An error occurred: Fail to open database!");
+    }
+    qDebug() << "Database opened successfully";
+//    MainWindow w;
+//    w.show();
+
+//    DoctorPage *d = new DoctorPage;
+//    d->show();
+
+    RecordItemPage *r = new RecordItemPage;
+    r->show();
     return a.exec();
 }
