09b72be79c6767a378beb6b6a6a94655ec25ed9c
CFNeko <55338055+CFNeko@users.noreply.github.com>
2023-08-26 17:32:01 +0800

    Revert "Merge pull request #9 from CFNeko/CoriFrontEnd"
    This reverts commit 8500be80bc19388e6f0ae778a7c38cb0cef28ebe, reversing
    changes made to 6795fa947cda9981e1347361132257e67bdc05bd.

--- main.cpp
+++ main.cpp
@@ -1,28 +1,11 @@
-#include "mainwindow.h"
-#include "doctorpage.h"
-#include "recorditempage.h"
+#include "registerweb.h"
 
 #include <QApplication>
-#include <QtSql>
-#include <QMessageBox>
-#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setDatabaseName("/home/user/Qtworks/qq_tiyanfu/database/database.db");
-    if (!db.open()) {
-        QMessageBox::critical(nullptr, "Error", "An error occurred: Fail to open database!");
-    }
-    qDebug() << "Database opened successfully";
-//    MainWindow w;
-//    w.show();
-
-//    DoctorPage *d = new DoctorPage;
-//    d->show();
-
-    RecordItemPage *r = new RecordItemPage;
-    r->show();
+    RegisterWeb w;
+    w.show();
     return a.exec();
 }
