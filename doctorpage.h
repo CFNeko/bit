7998e598c052bd502db38e637b4f2ad9722fddd6
FrankCori <129750838+FrankCori@users.noreply.github.com>
2023-08-28 09:41:00 +0800

    uodate doctor part
    666

--- doctorpage.h
+++ doctorpage.h
@@ -2,19 +2,24 @@
 #define DOCTORPAGE_H
 
 #include <QWidget>
-#include <QtSql>
-#include <QStackedWidget>
-#include <QTableWidget>
-#include "userfriendlyitems.h"
-#include "registrationpage.h"
+#include <QMouseEvent>
+#include"listtemplate.h"
+#include<QLineEdit>
+#include<QComboBox>
+#include<QTextEdit>
+#include<QTextEdit>
+#include<QDebug>
+#include<QPainter>
+#include"prescriptioninquiry.h"
+#include "recorditemtable.h"
+#include "prescriptiontable.h"
+#include "recorditemhistoryofdoctor.h"
+#include "registraitondoctorpage.h"
 
 namespace Ui {
 class DoctorPage;
 }
 
-
-
-//医生端界面类
 class DoctorPage : public QWidget
 {
     Q_OBJECT
@@ -22,13 +27,47 @@ class DoctorPage : public QWidget
 public:
     explicit DoctorPage(QWidget *parent = nullptr);
     ~DoctorPage();
+    void addCustomWidget();
+    //左侧部分的代码
+        QLabel *UserNameLabel;
+        QLabel *NameLabel;
+        QLabel *SexLabel;
+        QLabel *DepartmentLabel;
+        QLabel *AgeLabel;
+        QLabel *OtherLabel;
+        QLineEdit *UserNameLineEdit;
+        QLineEdit *NameLineEdit;
+        QComboBox *SexComBox;
+        QLineEdit *DepartmentEdit;
+        QLineEdit *AgeEdit;
+        QGridLayout *LeftLayout;
+        //右侧部分的代码
+        QLabel *HeadLabel;
+        QLabel *HeadIconLabel;
+        QPushButton *UpdateHeadButn;
+        QHBoxLayout *TopRightLayout;
+        QLabel *IntroductionLabel;
+        QTextEdit *IntroductionText;
+        QVBoxLayout *RightLayout;
+
+
+        //底部部分
+        QPushButton *OKBtn;
+        QPushButton *CancelBtn;
+        QHBoxLayout *BtnLayout;
+
+        void paintEvent(QPaintEvent *);
+
+
+
+private slots:
+        void on_prescription_inquiry_clicked();
+        void on_PreToHome_Received();
 
 private:
     Ui::DoctorPage *ui;
-    QStackedWidget *Pages = new QStackedWidget;
-    CustomNavigationBar *NavigationBar = new CustomNavigationBar(Pages);
-    QFrame *mainFrame;
-    int *DoctorID;
+    ListTemplate* m_pListTemplate;
+     PrescriptionInquiry* prescription_inquiry;
 
 };
 
